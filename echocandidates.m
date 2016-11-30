f13154303d27a61adb995e4a514585ce8faa240d
Raj Korpan <raj.m.korpan@gmail.com>
2016-11-29 23:25:25 -0500

    testing script to run algorithm

--- echocandidates.m
+++ echocandidates.m
@@ -9,6 +9,7 @@ zeroedcrosscorr(1:idx) = 0;
 pks2 = pks2(pks2>mean(pks2));
 locs2 = locs2(1:size(pks2,1));
 TOA = (locs(locs2)-size(RIR,1))/Fs;
+TOA = TOA(TOA>0);
 output = combnk(TOA,K);
 
 end
--- runalgorithm.m
+++ runalgorithm.m
@@ -1,5 +1,39 @@
-origSound = y;
-initialRIR = x1;
+origSound = origaudio;
+initialRIR = audio1;
+r1 = [0,0];
 K = 4;
-initialEchoCandidates = echocandidates(origSound,initialRIR,Fs,K);
+U = zeros(20,4);
 
+EchoCandidates1 = echocandidates(origSound,initialRIR,Fs,K);
+U(1,:) = mean(EchoCandidates1);
+
+
+r2 = [1.051,1.702];
+RIR2 = audio2;
+EchoCandidates2 = echolabelling(U(1,:),origSound,RIR2,Fs,K,r1,r2);
+U(2,:) = mean(EchoCandidates2(:,[2,4,6,8]));
+
+r3 = [1.867,3.528];
+RIR3 = audio3;
+EchoCandidates3 = echolabelling(U(2,:),origSound,RIR3,Fs,K,r2,r3);
+U(3,:) = mean(EchoCandidates3(:,[2,4,6,8]));
+
+r4 = [4.358,3.320];
+RIR4 = audio4;
+EchoCandidates4 = echolabelling(U(3,:),origSound,RIR4,Fs,K,r3,r4);
+U(4,:) = mean(EchoCandidates4(:,[2,4,6,8]));
+
+r5 = [3.557,3.918];
+RIR5 = audio5;
+EchoCandidates5 = echolabelling(U(4,:),origSound,RIR5,Fs,K,r4,r5);
+U(5,:) = mean(EchoCandidates5(:,[2,4,6,8]));
+
+r6 = [2.558,3.960];
+RIR6 = audio6;
+EchoCandidates6 = echolabelling(U(5,:),origSound,RIR6,Fs,K,r5,r6);
+U(6,:) = mean(EchoCandidates6(:,[2,4,6,8]));
+
+r7 = [1.548,5.686];
+RIR7 = audio7;
+EchoCandidates7 = echolabelling(U(6,:),origSound,RIR7,Fs,K,r6,r7);
+U(7,:) = mean(EchoCandidates7(:,[2,4,6,8]));
