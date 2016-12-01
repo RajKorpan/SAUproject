a1a351412639b55d26e8aa6f94340ab684b023bb
Raj Korpan <raj.m.korpan@gmail.com>
2016-12-01 13:19:24 -0500

    testing script

--- pointtolinedist.m
+++ pointtolinedist.m
@@ -1,6 +1,11 @@
-function [output] = pointtolinedist(linepoint1,linepoint2,point)
-x0 = 
-y0 = 
+function [output] = pointtolinedist(linepoint1,linepoint2,point,error)
+x0 = point(1,1);
+y0 = point(1,2);
+x1 = linepoint1(1,1);
+y1 = linepoint1(1,2);
+x2 = linepoint2(1,1);
+y2 = linepoint2(1,2);
 
+dist = abs((y2-y1)*x0-(x2-x1)*y0+x2*y1-y2*x1)/sqrt((y2-y1)^2+(x2-x1)^2);
+output = abs(normrnd(dist,error));
 end
-
--- tangentlines.m
+++ tangentlines.m
@@ -17,17 +17,22 @@ elseif nm1(3)<n(3)
     r1 = nm1(3);
     r0 = n(3);
 end
-xp = (c*r0-a*r1)/(r0-r1);
-yp = (d*r0-b*r1)/(r0-r1);
-xt1 = (((r0^2)*(xp-a)+(r0)*(yp-b)*sqrt((xp-a)^2+(yp-b)^2-(r0)^2))/((xp-a)^2+(yp-b)^2))+a;
-xt2 = (((r0^2)*(xp-a)-(r0)*(yp-b)*sqrt((xp-a)^2+(yp-b)^2-(r0)^2))/((xp-a)^2+(yp-b)^2))+a;
-yt1 = (((r0^2)*(yp-b)-(r0)*(xp-a)*sqrt((xp-a)^2+(yp-b)^2-(r0)^2))/((xp-a)^2+(yp-b)^2))+b;
-yt2 = (((r0^2)*(yp-b)+(r0)*(xp-a)*sqrt((xp-a)^2+(yp-b)^2-(r0)^2))/((xp-a)^2+(yp-b)^2))+b;
-slope1 = (yt1-yp)/(xt1-xp);
-slope2 = (yt2-yp)/(xt2-xp);
-b1 = yt1-(slope1*xt1);
-b2 = yt2-(slope2*xt2);
-%output = [a,b,c,d,r0,r1,xp,yp,xt1,yt1,xt2,yt2,slope1,b1,slope2,b2];
-output = [slope1,b1;slope2,b2];
+distcenters = sqrt((c-a)^2+(d-b)^2);
+if distcenters>r0
+    xp = (c*r0-a*r1)/(r0-r1);
+    yp = (d*r0-b*r1)/(r0-r1);
+    xt1 = (((r0^2)*(xp-a)+(r0)*(yp-b)*sqrt((xp-a)^2+(yp-b)^2-(r0)^2))/((xp-a)^2+(yp-b)^2))+a;
+    xt2 = (((r0^2)*(xp-a)-(r0)*(yp-b)*sqrt((xp-a)^2+(yp-b)^2-(r0)^2))/((xp-a)^2+(yp-b)^2))+a;
+    yt1 = (((r0^2)*(yp-b)-(r0)*(xp-a)*sqrt((xp-a)^2+(yp-b)^2-(r0)^2))/((xp-a)^2+(yp-b)^2))+b;
+    yt2 = (((r0^2)*(yp-b)+(r0)*(xp-a)*sqrt((xp-a)^2+(yp-b)^2-(r0)^2))/((xp-a)^2+(yp-b)^2))+b;
+    slope1 = (yt1-yp)/(xt1-xp);
+    slope2 = (yt2-yp)/(xt2-xp);
+    b1 = yt1-(slope1*xt1);
+    b2 = yt2-(slope2*xt2);
+    %output = [a,b,c,d,r0,r1,xp,yp,xt1,yt1,xt2,yt2,slope1,b1,slope2,b2];
+    output = [slope1,b1;slope2,b2];
+else
+    output = [];
+end
 end
 
